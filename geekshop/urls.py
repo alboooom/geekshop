85dbe417da2ec90da8f4c854f6da7dcd9b0e4d6e
Альберт Астадурьян <albertastaduran@Alberts-MacBook-Air.local>
2020-10-29 23:01:50 +0300

    IMPROVE: add template inheritance

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainapp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', views.products),
-    path('contact/', views.contact),
-    path('', views.main)
-]
+    path('', include('mainapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from . import models
 
+from django.http import HttpResponse
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    data = {
+        'products': models.Products.objects.all(),
+        'categories': models.Category.objects.all()
+    }
+    return render(request, 'mainapp/index.html', {'data': data})
 
 
 def products(request):
-    return render(request, 'mainapp/product.html')
+    data = {
+        'products': models.Products.objects
+    }
+    return render(request, 'mainapp/product.html', {'ds': data})
 
 
 def contact(request):
